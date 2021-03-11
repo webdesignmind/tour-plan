823de18b4295f01bd0d8f526ccb032bbce4d0e12
webdesignmind <axenoffdenis@yandex.ru>
2021-03-11 15:55:14 +0300

    create: adaptive version for max-width 1100-960px

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const swiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button__next',
