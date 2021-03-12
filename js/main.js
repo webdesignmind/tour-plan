343a0fc5c76d837d9ac40263ef37cf0233d562f1
webdesignmind <axenoffdenis@yandex.ru>
2021-03-12 18:20:55 +0300

    create: map api yandex

--- js/main.js
+++ js/main.js
@@ -13,3 +13,37 @@ const swiper = new Swiper('.swiper-container', {
     enabled: true,
   }
 });
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [-8.825719, 115.218629],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+    // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [-8.825719, 115.218629]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                iconContent: 'Hilton Phuket Arcadia',
+                hintContent: 'Ну давай уже тащи'
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        })
+    myMap.geoObjects
+        .add(myGeoObject)
+        ;
+}
