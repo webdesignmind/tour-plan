adc5df74b0a3bd9177b23a8d742e3bd3df249f17
webdesignmind <axenoffdenis@yandex.ru>
2021-03-23 12:55:44 +0300

    change: added adaptive rules for modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSwiper = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  var hotelSwiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   slidesPerView: 1,
@@ -13,7 +14,7 @@ var hotelSwiper = new Swiper('.hotel-slider', {
     enabled: true,
   }
 });
-var reviewsSwiper = new Swiper('.reviews-slider', {
+  var reviewsSwiper = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
   slidesPerView: 1,
@@ -25,12 +26,9 @@ var reviewsSwiper = new Swiper('.reviews-slider', {
   },
 });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-  .querySelector(".navbar-bottom")
-  .classList.toggle("navbar-bottom--visible");
+var menuButton = $(".menu-button");
+menuButton.on("click", function () {
+  $(".navbar-bottom").toggleClass("navbar-bottom--visible");
 });
 
 ymaps.ready(init);
@@ -68,3 +66,24 @@ function init() {
 }
 
 $('.parallax-window').parallax({imageSrc: '../img/newsletter-bg.jpeg'});
+
+var modalButton = $('[data-toggle="modal"]');
+var closeModalButton = $('.modal__close');
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal() { 
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+
+function closeModal(event) { 
+  event.preventDefault()
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
+});
