bcacf2c130672dbffd75b6baff5d09f2ca025852
webdesignmind <axenoffdenis@yandex.ru>
2021-03-23 17:22:06 +0300

    change: changed main.js

--- js/main.js
+++ js/main.js
@@ -1,90 +1,123 @@
 $(document).ready(function () {
-  var hotelSwiper = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 20,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: 'slide',
-  keyboard: {
-    enabled: true,
+  var hotelSwiper = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    effect: "slide",
+    keyboard: {
+      enabled: true,
+    },
+  });
+  var reviewsSwiper = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map(
+        "map",
+        {
+          center: [-8.825719, 115.218629],
+          zoom: 15,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }
+      ),
+      // Создаем геообъект с типом геометрии "Точка".
+      myGeoObject = new ymaps.GeoObject(
+        {
+          // Описание геометрии.
+          geometry: {
+            type: "Point",
+            coordinates: [-8.825719, 115.218629],
+          },
+          // Свойства.
+          properties: {
+            // Контент метки.
+            iconContent: "Hilton Phuket Arcadia",
+          },
+        },
+        {
+          // Опции.
+          // Иконка метки будет растягиваться под размер ее содержимого.
+          preset: "islands#blackStretchyIcon",
+          // Метку можно перемещать.
+          draggable: true,
+        }
+      );
+    myMap.geoObjects.add(myGeoObject);
   }
-});
-  var reviewsSwiper = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 20,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
 
-var menuButton = $(".menu-button");
-menuButton.on("click", function () {
-  $(".navbar-bottom").toggleClass("navbar-bottom--visible");
-});
+  $(".parallax-window").parallax({ imageSrc: "../img/newsletter-bg.jpeg" });
 
-ymaps.ready(init);
+  // var modalButton = $('[data-toggle="modal"]');
+  // var closeModalButton = $('.modal__close');
+  // modalButton.on('click', openModal);
+  // closeModalButton.on('click', closeModal);
 
-function init() {
-    var myMap = new ymaps.Map("map", {
-            center: [-8.825719, 115.218629],
-            zoom: 15
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+  // function openModal() {
+  //   var modalOverlay = $('.modal__overlay');
+  //   var modalDialog = $('.modal__dialog');
+  //   modalOverlay.addClass('modal__overlay--visible');
+  //   modalDialog.addClass('modal__dialog--visible');
+  // }
 
-    // Создаем геообъект с типом геометрии "Точка".
-        myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
-            geometry: {
-                type: "Point",
-                coordinates: [-8.825719, 115.218629]
-            },
-            // Свойства.
-            properties: {
-                // Контент метки.
-                iconContent: 'Hilton Phuket Arcadia',
-            }
-        }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
-            preset: 'islands#blackStretchyIcon',
-            // Метку можно перемещать.
-            draggable: true
-        })
-    myMap.geoObjects
-        .add(myGeoObject)
-        ;
-}
+  // function closeModal(event) {
+  //   event.preventDefault()
+  //   var modalOverlay = $('.modal__overlay');
+  //   var modalDialog = $('.modal__dialog');
+  //   modalOverlay.removeClass('modal__overlay--visible');
+  //   modalDialog.removeClass('modal__dialog--visible');
+  // }
 
-$('.parallax-window').parallax({imageSrc: '../img/newsletter-bg.jpeg'});
+  const activeClass = "active";
 
-var modalButton = $('[data-toggle="modal"]');
-var closeModalButton = $('.modal__close');
-modalButton.on('click', openModal);
-closeModalButton.on('click', closeModal);
-
-function openModal() { 
-  var modalOverlay = $('.modal__overlay');
-  var modalDialog = $('.modal__dialog');
-  modalOverlay.addClass('modal__overlay--visible');
-  modalDialog.addClass('modal__dialog--visible');
-}
-
-function closeModal(event) { 
-  event.preventDefault()
-  var modalOverlay = $('.modal__overlay');
-  var modalDialog = $('.modal__dialog');
-  modalOverlay.removeClass('modal__overlay--visible');
-  modalDialog.removeClass('modal__dialog--visible');
-}
-
+  // data-active
+  const activeBlockList = document.querySelectorAll("[data-active-block]");
+  if (activeBlockList) {
+    for (const activeBlock of activeBlockList) {
+      const activeControlList = document.querySelectorAll(
+        `[data-active-control="${activeBlock.dataset.activeBlock}"]`
+      );
+      for (const activeControl of activeControlList) {
+        function toggle() {
+          for (const activeControlItem of activeControlList)
+            activeControlItem.classList.toggle(activeClass);
+          activeBlock.classList.toggle(activeClass);
+        }
+        activeControl.addEventListener("click", () => {
+          toggle();
+        });
+        document.addEventListener("keydown", (event) => {
+          if (
+            event.code == "Escape" &&
+            activeBlock.classList.contains(activeClass)
+          ) {
+            toggle();
+          }
+        });
+      }
+    }
+  }
 });
