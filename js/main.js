6b5397af93d7289611fd9a8105cffac85df22dd9
webdesignmind <axenoffdenis@yandex.ru>
2021-03-25 11:05:07 +0300

    change: fixed adaptive for packages section

--- js/main.js
+++ js/main.js
@@ -69,27 +69,8 @@ $(document).ready(function () {
     myMap.geoObjects.add(myGeoObject);
   }
 
+  // Пралакс эффект
   $(".parallax-window").parallax({ imageSrc: "../img/newsletter-bg.jpeg" });
-
-  // var modalButton = $('[data-toggle="modal"]');
-  // var closeModalButton = $('.modal__close');
-  // modalButton.on('click', openModal);
-  // closeModalButton.on('click', closeModal);
-
-  // function openModal() {
-  //   var modalOverlay = $('.modal__overlay');
-  //   var modalDialog = $('.modal__dialog');
-  //   modalOverlay.addClass('modal__overlay--visible');
-  //   modalDialog.addClass('modal__dialog--visible');
-  // }
-
-  // function closeModal(event) {
-  //   event.preventDefault()
-  //   var modalOverlay = $('.modal__overlay');
-  //   var modalDialog = $('.modal__dialog');
-  //   modalOverlay.removeClass('modal__overlay--visible');
-  //   modalDialog.removeClass('modal__dialog--visible');
-  // }
 
   const activeClass = "active";
 
@@ -120,4 +101,25 @@ $(document).ready(function () {
       }
     }
   }
+
+  //Обработка форм (валидация)
+  //jqueryv
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Name must be at least 2 letters",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please enter your phone",
+        },
+      },
+    });
+  });
 });
